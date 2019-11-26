2f6d4632065d4be2fdc730a706ad23565b365bd9
Scott Chapman <scottbchapman@gmail.com>
2019-11-26 10:44:43 -0600

    Additional Styling

---
+++ src/app/renterapps/renterapps-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'pm-renters',
+    templateUrl: './renterapps-list.component.html'
+}
+)
+
+export class RentersappsListComponent {
+    pageTitle: string = 'Renter\'s Applications';
+}
